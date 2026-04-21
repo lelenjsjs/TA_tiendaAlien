77e84f2d5f37ab5838ee10bb08994bf70b440094
balvarezr <balvarezr@pucp.edu.pe>
2026-04-21 17:46:45 -0500

    package logistica e usuarios

---
+++ alienigenagames-model/src/main/java/pe/edu/pucp/tiendaalien/logistica/ModalidadPago.java
@@ -0,0 +1,7 @@
+package pe.edu.pucp.tiendaalien.logistica;
+
+public enum ModalidadPago {
+    PAGO_WEB,
+    PAGO_DESTINO,
+    A_COORDINAR
+}
---
+++ alienigenagames-model/src/main/java/pe/edu/pucp/tiendaalien/logistica/PedLogistica.java
@@ -0,0 +1,30 @@
+package pe.edu.pucp.tiendaalien.logistica;
+
+public class PedLogistica {
+    private int pedLogisticaId;
+    private EstadoLogistica estadoLogistica;
+    private String receptorNombre;
+    private String receptorTipoDoc;
+    private String receptorNroDoc;
+    private String receptorCel;
+
+    public PedLogistica() {}
+
+    public int getPedLogisticaId() { return pedLogisticaId; }
+    public void setPedLogisticaId(int pedLogisticaId) { this.pedLogisticaId = pedLogisticaId; }
+
+    public EstadoLogistica getEstadoLogistica() { return estadoLogistica; }
+    public void setEstadoLogistica(EstadoLogistica estadoLogistica) { this.estadoLogistica = estadoLogistica; }
+
+    public String getReceptorNombre() { return receptorNombre; }
+    public void setReceptorNombre(String receptorNombre) { this.receptorNombre = receptorNombre; }
+
+    public String getReceptorTipoDoc() { return receptorTipoDoc; }
+    public void setReceptorTipoDoc(String receptorTipoDoc) { this.receptorTipoDoc = receptorTipoDoc; }
+
+    public String getReceptorNroDoc() { return receptorNroDoc; }
+    public void setReceptorNroDoc(String receptorNroDoc) { this.receptorNroDoc = receptorNroDoc; }
+
+    public String getReceptorCel() { return receptorCel; }
+    public void setReceptorCel(String receptorCel) { this.receptorCel = receptorCel; }
+}
