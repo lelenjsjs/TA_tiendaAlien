2fdbfa195ee9da101eab58a9f7d35205698f5b27
Pris1720 <a20234897@pucp.edu.pe>
2026-04-22 03:06:18 -0500

    prueba ubigeo

--- App/src/main/java/pe/edu/pucp/tiendaalien/Main.java
+++ App/src/main/java/pe/edu/pucp/tiendaalien/Main.java
@@ -13,15 +13,46 @@ import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            UbigeoDAO ubiDAO = new UbigeoDAOImpl();
-            List<Ubigeo> lista = ubiDAO.listAll(); // Solo lee, no escribe
+    private static UbigeoDAO ubiDAO;
 
-            System.out.println("Conexión exitosa.");
-            System.out.println("Registros encontrados: " + lista.size());
-        } catch (Exception e) {
-            System.out.println("Error de conexión: " + e.getMessage());
-        }
+    public static void main(String[] args) {
+
+        // PRUEBA 1: UBIGEO
+        System.out.println("\n>> [TEST UBIGEO]");
+        UbigeoDAO ubiDAO = new UbigeoDAOImpl();
+
+        // A. INSERTAR (save)
+        Ubigeo nuevoUbi = new Ubigeo();
+        nuevoUbi.setCodigo("150101");
+        nuevoUbi.setDepartamento("Lima");
+        nuevoUbi.setProvincia("Lima");
+        nuevoUbi.setDistrito("Lima Cercado");
+        ubiDAO.save(nuevoUbi);
+        System.out.println("1. INSERTAR: Éxito. ID generado: " + nuevoUbi.getUbigeoId());
+
+        // B. OBTENER POR ID (load)
+        Ubigeo ubiCargado = ubiDAO.load(nuevoUbi.getUbigeoId());
+        System.out.println("2. OBTENER POR ID: Cargado: " + ubiCargado.getDistrito());
+
+        // C. MODIFICAR (update)
+        ubiCargado.setDistrito("Lima Cercado - MODIFICADO");
+        ubiDAO.update(ubiCargado);
+        System.out.println("3. MODIFICAR: Distrito cambiado a: " + ubiDAO.load(nuevoUbi.getUbigeoId()).getDistrito());
+
+        // D. LISTAR (listAll)
+        List<Ubigeo> listaUbi = ubiDAO.listAll();
+        System.out.println("4. LISTAR: Total de registros encontrados: " + listaUbi.size());
+
+        // E. ELIMINAR (remove)
+//        UbigeoDAO dao = new UbigeoDAOImpl();
+//        Ubigeo ubiAEliminar = new Ubigeo();
+//
+//        ubiAEliminar.setUbigeoId(2); // Pon aquí el número que quieres borrar
+//        dao.remove(ubiAEliminar);
+//
+//        System.out.println("Registro borrado con éxito desde Java.");
+
+        //  ========================================================================
+
     }
 }
